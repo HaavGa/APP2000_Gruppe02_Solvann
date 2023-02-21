908c5fcbc0e4a54bde1aea8ac9b29d780b2f6baa
marihans <124291557+marihans@users.noreply.github.com>
2023-02-21 12:10:35 +0100

    Get powerprice and solar from API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ApiData from "./components/ApiData";
 
 function App() {
   // random testkode
@@ -11,13 +12,14 @@ function App() {
   return (
     <>
       <div className="flex h-screen flex-col items-center justify-center bg-gray-700">
-        {visible && <h1 className="text-3xl text-white">Hallo!</h1>}
+        {/* {visible && <h1 className="text-3xl text-white">Hallo!</h1>}
         <button
           onClick={toggle}
           className="btn translate-y-10 bg-red-300 hover:bg-red-400"
         >
           Trykk
-        </button>
+        </button> */}
+        <ApiData />
       </div>
     </>
   );
