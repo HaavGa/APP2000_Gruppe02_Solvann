9470b676679351b718e3e4dc94f008bba6306463
Haavard Garsrud <havard.garsrud@gmail.com>
2023-02-21 14:49:02 +0100

    Code cleanup v2, add files to gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,11 @@ import { useState } from "react";
 import ApiData from "./components/ApiData";
 
 function App() {
-  // random testkode
   const [visible, setVisible] = useState(false);
-
-  const toggle = () => {
-    setVisible((prevVisible) => !prevVisible);
-  };
 
   return (
     <>
       <div className="flex h-screen flex-col items-center justify-center bg-gray-700">
-        {/* {visible && <h1 className="text-3xl text-white">Hallo!</h1>}
-        <button
-          onClick={toggle}
-          className="btn translate-y-10 bg-red-300 hover:bg-red-400"
-        >
-          Trykk
-        </button> */}
         <ApiData />
       </div>
     </>
