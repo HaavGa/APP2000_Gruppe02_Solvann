bd2852b2cd4c366d7a041e87caabdcf3586cc8a8
Emil <emilw.strand@gmail.com>
2023-02-26 22:28:29 +0100

    deployment

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const app = express();
 mongoose.set("strictQuery", false);
 connectDB();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 app.use(errorHandler);
