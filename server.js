6a2b8eaae2e316cc1a9a8b5598e6642d779bcff8
Emil <emilw.strand@gmail.com>
2023-02-26 23:11:03 +0100

    deployment

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ import { connectDB } from "./config/db.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 import waterLevelRoutes from "./routes/waterLevelRoutes.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//import path from 'path';
+//import { fileURLToPath } from 'url';
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
@@ -19,17 +19,19 @@ mongoose.set("strictQuery", false);
 connectDB();
 
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 app.use(errorHandler);
+
 app.use("/api/version/users", userRoutes);
 app.use("/api/version/waterLevel", waterLevelRoutes);
-
+/*
 app.use(express.static(path.join(__dirname, "./client/dist")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
-
+*/
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
