3a300334c213424b2b9cfac7f0898f9c0554998a
Emil <emilw.strand@gmail.com>
2023-02-26 21:39:06 +0100

    deployment

--- server.js
+++ server.js
@@ -8,19 +8,25 @@ import { errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 import waterLevelRoutes from "./routes/waterLevelRoutes.js";
 
-const port = process.env.PORT || 5000;
-
 dotenv.config();
+
 const app = express();
-console.log(process.env.MONGO_URI);
+
 mongoose.set("strictQuery", false);
 connectDB();
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}))
+
 app.use(errorHandler);
 app.use("/api/version/users", userRoutes);
 app.use("/api/version/waterLevel", waterLevelRoutes);
 
+app.use(express.static(path.join(__dirname, "./client/")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/index.html"));
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
