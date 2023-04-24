a54ef24bb4f4a8f3693534bfc46792f604b16a02
Haavard Garsrud <havard.garsrud@gmail.com>
2023-04-25 01:34:25 +0200

    Add loading spinner

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div class="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
