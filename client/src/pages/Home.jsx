5ad8a2166507b3dda7e42c56e4046d28c2a5992b
Haavard Garsrud <havard.garsrud@gmail.com>
2023-04-25 01:31:35 +0200

    Move SignupForm to Admin page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import ApiData from "../components/ApiData";
-import SignupForm from "../components/SignupForm";
 
 const Home = () => {
   return (
     <>
       <div className="grid h-screen grid-cols-1 items-center bg-gray-700 lg:grid-cols-2">
         <ApiData />
-        <div className="mx-auto">
-          <SignupForm />
-        </div>
         <button className="btn mx-auto bg-red-500 hover:bg-red-600">
           Stopp
         </button>
