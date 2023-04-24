9f7aaf3a274606c7d26c41330eb6c0c4ff429697
Haavard Garsrud <havard.garsrud@gmail.com>
2023-04-25 01:34:16 +0200

    Add list of current users with pagination

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+import SignupForm from "../components/admin/SignupForm";
+import UsersList from "../components/admin/UsersList";
+import Axios from "axios";
 
 const Admin = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+      const response = await Axios.get(
+        "http://localhost:5000/api/version/users/"
+      );
+      setUsers(response.data);
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="h-screen bg-gray-700 py-5 text-center text-white">
-      <h1 className="text-4xl">Admin</h1>
+    <div className="h-screen bg-gray-700 py-5 text-center">
+      <div className="mt-10 flex gap-2 ">
+        <UsersList users={users} loading={loading} />
+        <SignupForm />
+      </div>
     </div>
   );
 };
