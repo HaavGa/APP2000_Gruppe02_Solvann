76b776be7989bc5bc6ebc4aacbdc0c4b2e880d52
Håvard Garsrud <havard.garsrud@gmail.com>
2023-04-25 23:54:47 +0200

    Change "class" to "className"

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div class="lds-spinner">
+    <div className="lds-spinner">
       <div></div>
       <div></div>
       <div></div>
