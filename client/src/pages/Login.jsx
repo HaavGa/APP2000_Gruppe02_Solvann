3a8f59655e5708b2da037ee1c3f82e6d74e80bba
Håvard Garsrud <havard.garsrud@gmail.com>
2023-05-23 13:28:40 +0200

    Update URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,12 +18,12 @@ const Login = () => {
     },
   });
   const submitHandler = async (values) => {
-    const baseUrl = "https:solvann.cyclic.app/api/users/auth";
+    const baseUrl = "https://solvann.cyclic.app/api/users/auth";
     const data = values;
     try {
-      const response = await axios.post(`${baseUrl}`, data);
-      console.log(window.location.hostname);
       console.log(import.meta.env.PROD);
+      console.log(window.location.hostname);
+      const response = await axios.post(`${baseUrl}`, data);
 
       signIn({
         token: response.data.token,
@@ -36,7 +36,7 @@ const Login = () => {
     }
   };
   return (
-    <div className="bg-login flex h-screen w-full items-center justify-center bg-cover bg-no-repeat">
+    <div className="flex h-screen w-full items-center justify-center bg-login bg-cover bg-no-repeat">
       <div className="max-sm:px-8 rounded-xl bg-gray-800/50 px-16 py-10 shadow-lg backdrop-blur-md">
         <div className="text-white">
           <div className="mb-8 flex flex-col items-center">
