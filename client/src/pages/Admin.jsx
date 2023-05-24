18a578fb77ecfa69751db2801dcaa1ab5340306f
Håvard Garsrud <havard.garsrud@gmail.com>
2023-05-24 12:48:30 +0200

    Add UpdateUser form

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -7,30 +7,69 @@ import axios from "axios";
 const Admin = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [updatingUser, setUpdatingUser] = useState(false);
+  const [loadingEdit, setLoadingEdit] = useState(false);
+  const [updateForm, setUpdateForm] = useState(false);
+  const [savedValues, setSavedValues] = useState(null);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    (async () => {
       setLoading(true);
       const response = await axios.get(
         "https://solvann.cyclic.app/api/users/all"
       );
       setUsers(response.data);
       setLoading(false);
-    };
-    fetchUsers();
+    })();
   }, []);
 
-  const updateUser = () => {
-    setUpdatingUser(true);
+  const updateUser = async (_id) => {
+    console.log(_id);
+    setUpdateForm(true);
+    setLoadingEdit(true);
+    try {
+      const response = await axios.patch(
+        "https://solvann.cyclic.app/api/users/profile",
+        { id: _id }
+      );
+      setSavedValues(response.data);
+      setLoadingEdit(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // const deleteUser = async (_id) => {
+  //   console.log(_id);
+  //   try {
+  //     await axios.delete("https://solvann.cyclic.app/api/users/delete", {
+  //       id: _id,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  const deleteUser = async (_id) => {
+    console.log(`${_id} is deleted`);
   };
 
   return (
-    <div className="h-screen bg-gray-700 py-5 text-center">
-      <div className="mt-10 flex gap-2 ">
-        <UsersList users={users} loading={loading} updateUser={updateUser} />
-        {!updatingUser && <SignupForm />}
-        {updatingUser && <UpdateForm />}
+    <div className="bg-gray-700 py-3 text-center">
+      <div className="mt-10 flex justify-center gap-2 ">
+        <UsersList
+          users={users}
+          loading={loading}
+          updateUser={updateUser}
+          deleteUser={deleteUser}
+        />
+        {!updateForm && <SignupForm />}
+        {savedValues && updateForm && (
+          <UpdateForm
+            loadingEdit={loadingEdit}
+            setUpdateForm={setUpdateForm}
+            savedValues={savedValues}
+          />
+        )}
       </div>
     </div>
   );
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import userRoutes from './routes/userRoutes.js';
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import {
+  notFound,
+  errorHandler,
+} from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 import waterLevelRoutes from "./routes/waterLevelRoutes.js";
 import turbineRoutes from "./routes/turbineRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
@@ -16,23 +19,32 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(
+  cors({
+    origin: [
+      "https://solvann.cyclic.app/api/users/new",
+      "https://solvann.cyclic.app",
+    ],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/water", waterLevelRoutes);
 app.use("/api/turbine", turbineRoutes);
-app.use("/api/admin", adminRoutes)
+app.use("/api/admin", adminRoutes);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "client", "dist", "index.html")
+  )
 );
 
 app.use(notFound);
