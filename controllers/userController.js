697341b3e94844e7adcf72e9d90e480168c9582e
Emil <emilw.strand@gmail.com>
2023-05-25 14:43:42 +0200

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,29 +12,9 @@ const getUsers = asyncHandler(async (req, res) => {
 
   res.status(200).json(usersFound);
 });
-
-// @desc    Delete goal
-// @route   DELETE /api/version/users
-// @access  Private
-const deleteUser = asyncHandler(async (req, res) => {
-  const user = await User.findById({ _id: req.params.id });
-  if (!user) {
-    res.status(404);
-    throw new Error({ Error: "User not found." });
-  }
-
-  const userDeleted = await user.remove();
-
-  if (!userDeleted) {
-    res.status(404);
-    throw new Error({ Error: "User could not be deleted." });
-  }
-
-  res.status(200).json(userDeleted);
-});
-
+/*
 const getUser = asyncHandler(async (req, res) => {
-  const user = await User.findById({ _id: req.body.id });
+  const user = await User.findById({ _id: req.params.id }).select('-password');
   if(!user){
     res.status(404);
     throw new Error("User not found.");
@@ -45,45 +25,9 @@ const getUser = asyncHandler(async (req, res) => {
     lastName: user.lastName,
     email: user.email,
     isAdmin: user.isAdmin,
+  });
 });
-});
-
-const getUserId = asyncHandler(async (req, res) => {
-  const email = req.body.email;
-  const userFound = await User.findOne({ email: email });
-  if(!userFound){
-    res.status(404);
-    throw new Error("User not found");
-  }
-
-  res.status(200).json({ id: userFound.id });
-
-});
-// @desc    Auth user & get token
-// @route   POST /api/users/auth
-// @access  Public
-const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-
-
-  if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id);
-    res.json({
-      id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: res.token
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
-  }
-});
-
+*/
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
@@ -123,39 +67,52 @@ const registerUser = asyncHandler(async (req, res) => {
   
 });
 
-
-
-// @desc    Logout user / clear cookie
-// @route   POST /api/users/logout
-// @access  Public
-const logoutUser = (req, res) => {
-  res.cookie('jwt', '', { 
-    httpOnly: true,
-    expires: new Date(0),
-  });
-  res.status(200).json({ message: 'Logged out successfully' });
-};
-
-// @desc    Get user profile
-// @route   GET /api/users/profile
+// @desc    Delete goal
+// @route   DELETE /api/version/users
 // @access  Private
-const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-
-  if(!user){
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById({ _id: req.params.id });
+  if (!user) {
     res.status(404);
-    throw new Error('User not found');
+    throw new Error({ Error: "User not found." });
   }
 
-  res.status(200).json({
-    _id: user._id,
-    firstName: user.name,
-    lastName:user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-  });
+  const userDeleted = await user.remove();
 
+  if (!userDeleted) {
+    res.status(404);
+    throw new Error({ Error: "User could not be deleted." });
+  }
+
+  res.status(200).json(userDeleted);
+});
+
+
+
+
+// @desc    Auth user & get token
+// @route   POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.json({
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: res.token
+    });
+  } else {
+    res.status(401);
+    throw new Error('Feil e-post eller passord!');
+  }
 });
 
 // @desc    Update user profile
@@ -191,13 +148,9 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 export {
-  getUserId,
   deleteUser,
-  getUser,
   authUser,
   registerUser,
-  logoutUser,
-  getUserProfile,
   updateUser,
   getUsers
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,24 +2,22 @@ import express from 'express';
 import {
   authUser,
   registerUser,
-  logoutUser,
-  getUserProfile,
   updateUser,
   getUsers,
   deleteUser,
-  getUser,
-  getUserId,
 } from '../controllers/userController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(protect, isAdmin, getUserId);
-router.route('/user').post(protect, isAdmin, getUser);
+router.route('/')
+  .get(protect, isAdmin, getUsers)
+  .post(protect, isAdmin, registerUser)
+  .patch(protect, isAdmin, updateUser);
 router.route('/:id').delete(protect, isAdmin, deleteUser)
-router.post('/auth', authUser);
-router.route('/new').post(protect, isAdmin, registerUser);
-router.route('/all').get(protect, isAdmin, getUsers);
-router.route('/profile').patch(protect, isAdmin, updateUser);
+router.post('/login', authUser);
+
+
+
 
 export default router;
