fba96c3beebe3936634f9ba5b6ab19fc209405d0
Håvard Garsrud <havard.garsrud@gmail.com>
2023-05-28 00:24:14 +0200

    Add live data on homepage

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv("hello", process.cwd(), "");
+  const processEnvValues = {
+    "process.env": Object.entries(env).reduce((prev, [key, val]) => {
+      return {
+        ...prev,
+        [key]: val,
+      };
+    }, {}),
+  };
+
+  return {
+    plugins: [react()],
+    define: processEnvValues,
+  };
+});
