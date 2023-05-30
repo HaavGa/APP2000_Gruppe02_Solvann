ebb2adbdd4c8dbc5c8a37917988e51da614ca26b
marihans <124291557+marihans@users.noreply.github.com>
2023-05-30 21:55:04 +0200

    add dummy reports

--- client/src/pages/Rapporter.jsx
+++ client/src/pages/Rapporter.jsx
@@ -6,66 +6,78 @@ const Rapporter = () => {
   // dummy data for bruker-rapporten
   const userData = [
     {
-      _id: {
-        $oid: "646df520f02fef008f8ff5ad",
-      },
-      firstName: "John",
-      lastName: "Carew",
-      email: "jcarew@solvann.no",
-      password: "$2b$10$pIouKLAzhBExUjNHWG.5V.wdQVyOJ/.N5kKQU7Boa0LunwZpRKyfm",
-      isAdmin: false,
-      createdAt: {
-        $date: {
-          $numberLong: "1684927776750",
-        },
-      },
-      updatedAt: {
-        $date: {
-          $numberLong: "1685019261640",
-        },
-      },
-      __v: 0,
+      _id: "6476201146c30b73d65b1206",
+      firstName: "Endre",
+      lastName: "Adminsen",
+      email: "admin1@solvann.no",
+      isAdmin: true,
+      createdAt: "2023-05-30 16:10:57",
     },
     {
-      _id: {
-        $oid: "646e0202592d462b846589a1",
-      },
-      firstName: "Kari",
-      lastName: "Banan",
-      email: "kbanan@solvann.no",
-      password: "$2b$10$yw4xCXiqP0pO2KZvfS4bue8D7oZE9JZoT0POFDv/MOp3aVOJpGmfu",
+      _id: "6476206546c30b73d65b120b",
+      firstName: "Tone",
+      lastName: "Adminsen",
+      email: "admin2@solvann.no",
+      isAdmin: true,
+      createdAt: "2023-05-30 16:12:21",
+    },
+    {
+      _id: "6476208a46c30b73d65b1210",
+      firstName: "Trevor",
+      lastName: "Adminsen",
+      email: "admin3@solvann.no",
+      isAdmin: true,
+      createdAt: "2023-05-30 16:12:58",
+    },
+    {
+      _id: "647620bb46c30b73d65b1215",
+      firstName: "Enok",
+      lastName: "Driftesen",
+      email: "drift1@solvann.no",
       isAdmin: false,
-      createdAt: {
-        $date: {
-          $numberLong: "1684931074176",
-        },
-      },
-      updatedAt: {
-        $date: {
-          $numberLong: "1684931074176",
-        },
-      },
-      __v: 0,
-    }];
+      createdAt: "2023-05-30 16:13:47",
+    },
+    {
+      _id: "647620e846c30b73d65b121b",
+      firstName: "Tomine",
+      lastName: "Driftesen",
+      email: "drift2@solvann.no",
+      isAdmin: false,
+      createdAt: "2023-05-30 16:14:32",
+    },
+    {
+      _id: "6476210c46c30b73d65b1222",
+      firstName: "Trello",
+      lastName: "Driftesen",
+      email: "drift3@solvann.no",
+      isAdmin: false,
+      createdAt: "2023-05-30 16:15:08",
+    },
+  ];
 
   // dummy-data for vannstand-rapporten
   const waterlevelData = [
     {
-      "waterLevel": 50,
-      "date": {
-        "$date": {
-          "$numberLong": "1685052002629"
-        }
-      }
+      waterlevel: 40.051923674910746,
+      time: "2023-05-30 14:01:23",
     },
     {
-      "waterLevel": 27.167331326978715,
-      "date": {
-        "$date": {
-          "$numberLong": "1685059202660"
-        }
-      }
-  }];
+      waterlevel: 28.194045652296037,
+      time: "2023-05-30 16:01:48",
+    },
+    {
+      waterlevel: 14.175802582986169,
+      time: "2023-05-30 18:00:08",
+    },
+    {
+      waterlevel: 35.75984724020747,
+      time: "2023-05-30 20:04:37",
+    },
+    {
+      waterlevel: 30.027601541462648,
+      time: "2023-05-30 22:02:13",
+    },
+  ];
 
   return (
     <div className="h-screen bg-gray-700 py-5 text-center text-white">
